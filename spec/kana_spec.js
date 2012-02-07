4ee2b4b2a0a3a2eebed2e504af29e7234df3ee2e
Sergey Kluchkowsky <kaineer@gmail.com>
2012-02-07 22:56:29 +0600

    Initial commit

---
+++ spec/kana_spec.js
@@ -0,0 +1,64 @@
+//
+//
+//
+describe("Kana", function() {
+  var kana;
+
+  beforeEach(function() {
+    this.addMatchers({
+      toHaveMethod: function(methodName) {
+        return typeof(this.actual[methodName]) == "function";
+      }
+    });
+  });
+
+  describe("initialized without parameters", function() {
+    beforeEach(function() {
+      kana = new Kana();
+    });
+
+    it("should respond to stringify", function() {
+      // expect(typeof(kana.stringify)).toEqual("function");
+      expect(kana).toHaveMethod("stringify");
+    });
+
+    it("should respond to htmlize", function() {
+      expect(kana).toHaveMethod("htmlize");
+    });
+
+    it("should respond to set", function() {
+      expect(kana).toHaveMethod("set");
+    })
+  });
+
+  describe("initialized with romaji", function() {
+    beforeEach(function() {
+      kana = new Kana({romaji: "sakana"});
+    });
+
+    it("should keep romaji", function() {
+      expect(kana.romaji).toEqual("sakana");
+    });
+  });
+
+  describe("set new value after initialization", function() {
+    var setValue = "nori";
+
+    beforeEach(function() {
+      kana = new Kana();
+      kana.set(setValue);
+    });
+
+    it("should keep new value", function() {
+      expect(kana.romaji).toEqual(setValue);
+    });
+  });
+
+  describe("standard kana tests", function() {
+    var cases = {
+      a: "\u3042",
+      i: "\u3044",
+      u: "\u3046"
+    };
+  });
+});
