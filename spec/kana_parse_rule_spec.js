d36f3abe41f7976b86990fd7180d91e4c6262104
Tangerine Cat <kaineer@gmail.com>
2012-02-29 15:53:06 +0600

    More specs to #handle

--- spec/kana_parse_rule_spec.js
+++ spec/kana_parse_rule_spec.js
@@ -42,7 +42,7 @@ describe("Kana.ParseRule", function() {
         expect(_.isArray(rule.matches)).toBeTruthy();
       });
 
-      it("should contain matches with first element equal to matched fragment", function() {
+      it("first matches element should be equal to matched fragment", function() {
         expect(rule.matches[0]).toEqual("A");
       });
     });
@@ -50,6 +50,93 @@ describe("Kana.ParseRule", function() {
 
   //
   describe("#handle", function() {
+    //
+    // Simple rule just with regexp
+    //
+    describe("/^BA/", function() {
+      beforeEach(function() {
+        rule = new ruleClass(/^BA/);
+        buffer = new bufferClass("BAXXX");
+        rule.accepts(buffer);
 
+        spyOn(buffer, "push");
+        spyOn(buffer, "drop");
+
+        rule.handle(buffer);
+      });
+
+      it("should push BA to buffer", function() {
+        expect(buffer.push).toHaveBeenCalledWith("BA");
+      });
+
+      it("should drop 2 characters from source", function() {
+        expect(buffer.drop).toHaveBeenCalledWith(2);
+      });
+    });
+
+    describe("/^(B)BA/", function() {
+      beforeEach(function() {
+        rule = new ruleClass(/^(B)BA/);
+        buffer = new bufferClass("BBAXXX");
+        rule.accepts(buffer);
+
+        spyOn(buffer, "push");
+        spyOn(buffer, "drop");
+
+        rule.handle(buffer);
+      });
+
+      it("should push B to buffer", function() {
+        expect(buffer.push).toHaveBeenCalledWith("B");
+      });
+
+      it("should drop 1 character from buffer", function() {
+        expect(buffer.drop).toHaveBeenCalledWith(1);
+      });
+    });
+
+    describe("/^A/, :push => 'CD'", function() {
+      beforeEach(function() {
+        rule = new ruleClass(/^A/, {"push": "CD"});
+        buffer = new bufferClass("ABEF");
+        rule.accepts(buffer);
+
+        spyOn(buffer, "push");
+        spyOn(buffer, "drop");
+
+        rule.handle(buffer);
+      });
+
+      it("should push CD to buffer", function() {
+        expect(buffer.push).toHaveBeenCalledWith("CD");
+      });
+
+      it("should drop 1 character from buffer", function() {
+        expect(buffer.drop).toHaveBeenCalledWith(1);
+      });
+    });
+
+    describe("/^([AB])x([CD])/, :push => function() { ... }", function() {
+      beforeEach(function() {
+        rule = new ruleClass(/^([AB])x([CD])/, {"push": function(matches) {
+          return matches[1] + matches[2];
+        }});
+        buffer = new bufferClass("AxDFOOBAR");
+        rule.accepts(buffer);
+
+        spyOn(buffer, "push");
+        spyOn(buffer, "drop");
+
+        rule.handle(buffer);
+      });
+
+      it("should push AD to buffer", function() {
+        expect(buffer.push).toHaveBeenCalledWith("AD");
+      });
+
+      it("should drop 3 characters from buffer", function() {
+        expect(buffer.drop).toHaveBeenCalledWith(3);
+      });
+    });
   });
 });
