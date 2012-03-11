fbbc1101829e5ddc73b3cf202f1cbcc412db73f2
Tangerine Cat <kaineer@gmail.com>
2012-03-11 16:51:17 +0600

    Add romaji parser, step 3, imported all rules

--- spec/kana_romaji_parser_spec.js
+++ spec/kana_romaji_parser_spec.js
@@ -15,6 +15,7 @@ describe("Kana.RomajiParser", function() {
       {input: "sa", expected: ["SA"]},
       {input: "ki", expected: ["KI"]},
       {input: "go", expected: ["GO"]},
+      {input: "ru", expected: ["RU"]},
 
       // Vowel syllable
       {input: "a",  expected: ["A"]},
@@ -28,10 +29,18 @@ describe("Kana.RomajiParser", function() {
 
       // Multiply syllables
       {input: "sakana", expected: ["SA", "KA", "NA"]},
-      {input: "mikan",  expected: ["MI", "KA", "N"]},
+      {input: "mikan",  expected: ["MI", "KA", "N"]},   // TODO: check for end of string!!!
 
       // NYA
-      {input: "nya",    expected: ["NI", "ya"]}
+      {input: "nya",    expected: ["NI", "ya"]},
+      {input: "chi",    expected: ["CHI"]},
+      {input: "du",     expected: ["DZU"]},
+
+      {input: "shojo",     expected: ["SHI", "yo", "JI", "yo"]},
+
+      {input: "FORUKU",  expected: ["FU", "o", "RU", "KU"]},
+      {input: "etchi",   expected: ["E", "tsu", "CHI"]},
+      {input: "borupen", expected: ["BO", "RU", "PE", "N"]}
     ];
 
     _(cases).each(function(theCase) {
